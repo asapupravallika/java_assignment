413737bacd85c9c7aa0c2111c686cdbdc251a276
asapupravallika <163882439+asapupravallika@users.noreply.github.com>
2026-06-15 09:22:46 +0530

    feat: Sum of Elements in the Last Column

---
+++ SumOfLastColumn.java
@@ -0,0 +1,21 @@
+package com.codegnan.assignment;
+import java.util.Scanner;
+public class SumOfLastColumn {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		int c = sc.nextInt();
+		int[][] arr = new int[r][c];
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		int sum = 0;
+		for (int i = 0; i < r; i++) {
+					sum += arr[i][c-1];
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+}
